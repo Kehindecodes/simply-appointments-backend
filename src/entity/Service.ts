0337520a0e2f04d28244a90bb4637f9f14911baa
gee-dev29 <maduegodwin2605@gmail.com>
2024-07-02 18:29:11 +0100

    added service

---
+++ src/entity/Service.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { currency } from "../enum/Currency";
+
+Entity();
+export class Service {
+    @PrimaryGeneratedColumn("uuid")
+    id?: string;
+
+    @Column()
+    serviceName?: string;
+
+    @Column()
+    description?: string;
+
+    @Column()
+    isActive?: boolean;
+
+    @Column() // use the currency enum here
+    currency?: currency;
+
+    @Column()
+    price?: number;
+
+    @Column()
+    duration?: number;
+    required?: [
+        "name",
+        "price"
+    ];
+    @Column()
+    availability?: boolean
+    default?: true
+
+    declare createdAt?: Date
+}
