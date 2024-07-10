36426cf674624332991686033248ae1032e2378e
Kehinde Oki <kehinde.oki@abbaandking.com>
2024-07-10 11:18:18 +0100

    fix

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { createService } from '../controller/ServiceController';
+// import { createService } from '../controller/ServiceController';
 const router = Router();
 
 router.post('/create-service',
-    createService
+    // createService
 )
