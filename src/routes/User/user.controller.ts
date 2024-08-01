84f275387a3404db1ac9fa85e07533de269de0a7
Kehinde Oki <kehinde.oki@abbaandking.com>
2024-08-01 16:52:22 +0100

    implement forgot password and refactor jwtauthentication

--- src/routes/User/user.controller.ts
+++ src/routes/User/user.controller.ts
@@ -12,6 +12,8 @@ import jwt from "jsonwebtoken";
 import { LessThan, MoreThan } from "typeorm";
 import moment from "moment-timezone";
 import { Role } from "../../entity/Role";
+import { generateToken, sendPasswordResetLink, verifyToken } from "../../utils/token.utils";
+import { LinkToken } from "../../entity/Token";
 
 export const registerUser = async (
     req: Request,
@@ -131,7 +133,7 @@ export const ValidateOTP = async (req: Request, res: Response) => {
         .tz(dbTimezone)
         .subtract(5, "minutes")
         .toDate();
-
+try{
     // find the latest unexpired OTP for the email
     const otpData = await AppDataSource.manager.findOne(OTP, {
         where: {
@@ -149,16 +151,25 @@ export const ValidateOTP = async (req: Request, res: Response) => {
 
     const user = await AppDataSource.manager.findOneBy(User, { email });
 
-    if (otp === DbOtp) {
-        // Generate JWT token
-        const token = jwt.sign({ user }, process.env.SECRET_KEY as string, {
-            algorithm: "HS256",
-        });
-        res.header("Authorization", `Bearer ${token}`);
-        return res.status(200).json({ message: "OTP validated successfully" });
-    }
-
-    return res.status(400).json({ message: "Invalid OTP" });
+    if(user){
+        if (otp === DbOtp) {
+            // Generate JWT token
+            const token = jwt.sign({ id: user.userId }, process.env.SECRET_KEY as string, {
+                algorithm: "HS256",
+                expiresIn: "1h",
+            });
+            res.header("Authorization", `Bearer ${token}`);
+            return res.status(200).json({ message: "OTP validated successfully" });
+        } 
+        return res.status(400).json({ message: "Invalid OTP" });
+    } 
+    return res.status(404).json({ message: "No user found" });
+} catch (err: any) {
+    res.status(500).send({
+        message: "Error validating OTP",
+    })
+}
+    
 };
 
 export const getUsersWithRole = async (
@@ -179,3 +190,58 @@ export const getUsersWithRole = async (
         });
     }
 };
+
+export const forgotPassword = async (req: Request, res: Response) : Promise<void> => {
+    const { email } = req.body;
+    if (!email) {
+        res.status(400).json({message: "Email is required"});
+        return;
+    }
+    try {
+            await sendPasswordResetLink(email);
+            res.status(200).json({message: "Reset password link sent successfully"});
+
+    } catch (error) {
+        console.error(error); // Log the error for debugging purposes
+        res.status(500).json({message: "Error sending reset password link", error: error});
+    }
+}
+
+export const allowPasswordReset = async (req: Request, res: Response) : Promise<void> => {
+    const { token } = req.params;
+    try {
+        const storedToken = await AppDataSource.manager.findOne(LinkToken, { where: { token } });
+        if (!storedToken) {
+            res.status(401).json({ message: "Invalid or expired token. Please request a new token" });
+        }
+        res.status(200).json({ message: "Enter new password" });
+    } catch (err: any) {
+        res.status(500).send({
+            message: err.message,
+            error: err,
+        });
+    }
+}
+
+export const resetPassword = async (req: Request, res: Response) : Promise<Response> => {
+    const {password, confirmPassword, email} = req.body;
+    if (!password || !confirmPassword || !email) {
+        return res.status(400).json({message: "All fields are required"});
+    }
+    if (password !== confirmPassword) {
+       return res.status(400).json({message: "Passwords do not match"});
+    }
+    const user =   await AppDataSource.manager.findOne(User,{ where:{email} });
+    if(!user) {
+        return res.status(400).json({message: "wrong email"});
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const updatedUser = await AppDataSource.manager.update(User, {email}, {password: hashedPassword});
+    res.status(200).json({
+        message: "Password reset successfully",
+        updatedUser : updatedUser
+});
+    return res;
+
+
+}
--- src/routes/User/user.route.ts
+++ src/routes/User/user.route.ts
@@ -1,13 +1,23 @@
-import { Router } from 'express';
-import { checkUserExists } from '../../middlewares/checkUserExists';
-import { registerUser, loginUser, ValidateOTP, getUsersWithRole } from './user.controller';
+import { Router } from "express";
+import { checkUserExists } from "../../middlewares/checkUserExists";
+import {
+    registerUser,
+    loginUser,
+    ValidateOTP,
+    getUsersWithRole,
+    forgotPassword,
+    allowPasswordReset,
+    resetPassword,
+} from "./user.controller";
 // import { createService } from '../controller/ServiceController';
 const userRouter = Router();
 
-userRouter.post('/',checkUserExists, registerUser);
-userRouter.post('/login', loginUser);
-userRouter.post('/validate-otp', ValidateOTP);
-userRouter.get('/', getUsersWithRole);
-
+userRouter.post("/", checkUserExists, registerUser);
+userRouter.post("/login", loginUser);
+userRouter.post("/validate-otp", ValidateOTP);
+userRouter.get("/", getUsersWithRole);
+userRouter.post("/forgot-password", forgotPassword);
+userRouter.post("/reset-password", resetPassword);
+userRouter.get("/reset-password/:token", allowPasswordReset);
 
 export default userRouter;
