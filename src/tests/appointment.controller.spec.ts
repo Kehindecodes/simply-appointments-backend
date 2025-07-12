4ab944c8f4b907ed8763dbb82fe4f81352cc5563
Kehindecodes <olamiposi.oki@gmail.com>
2025-07-12 08:45:09 +0100

    comment out test files

--- src/tests/appointment.controller.spec.ts
+++ src/tests/appointment.controller.spec.ts
@@ -1,52 +1,52 @@
-import { getMockReq, getMockRes } from "@jest-mock/express";
-import { AppDataSource } from "../shared/database/migration/data-source";
-import { bookAppointment } from "../routes/Apppointment/appointment.controller";
-import { AppointmentStatus } from "../shared/config/enums/AppointmentStatus";
+// import { getMockReq, getMockRes } from "@jest-mock/express";
+// import { AppDataSource } from "../shared/database/migration/data-source";
+// import { bookAppointment } from "../routes/Apppointment/appointment.controller";
+// import { AppointmentStatus } from "../shared/config/enums/AppointmentStatus";
 
 
 
-describe("appointments", () => {
-    beforeAll(async () => {
-        jest.mock("../shared/database/migration/data-source");
-    });
+// describe("appointments", () => {
+//     beforeAll(async () => {
+//         jest.mock("../shared/database/migration/data-source");
+//     });
 
-    it("should create a new appointment", async () => {
-        const req = getMockReq({
-            body: {
-                userId: "123e4567-e89b-12d3-a456-426614174000",
-                time: new Date().toISOString(),
-                staffId: "123e4567-e89b-12d3-a456-426614174000",
-                serviceId: "123e4567-e89b-12d3-a456-426614174000",
-                date : new Date(),
-                status: AppointmentStatus.PENDING
-            }
-        });
-        const res = getMockRes().res;
+//     it("should create a new appointment", async () => {
+//         const req = getMockReq({
+//             body: {
+//                 userId: "123e4567-e89b-12d3-a456-426614174000",
+//                 time: new Date().toISOString(),
+//                 staffId: "123e4567-e89b-12d3-a456-426614174000",
+//                 serviceId: "123e4567-e89b-12d3-a456-426614174000",
+//                 date : new Date(),
+//                 status: AppointmentStatus.PENDING
+//             }
+//         });
+//         const res = getMockRes().res;
 
-        const mockSave = jest.fn().mockImplementation((appointment) => {
-            console.log("mockSave called with:", appointment);
-            return Promise.resolve({ ...appointment, id: '123e4567-e89b-12d3-a456-426614174000'});
-        });
+//         const mockSave = jest.fn().mockImplementation((appointment) => {
+//             console.log("mockSave called with:", appointment);
+//             return Promise.resolve({ ...appointment, id: '123e4567-e89b-12d3-a456-426614174000'});
+//         });
 
-        AppDataSource.manager.save = mockSave;
+//         AppDataSource.manager.save = mockSave;
 
-        await bookAppointment(req, res);
+//         await bookAppointment(req, res);
 
-        expect(mockSave).toHaveBeenCalledTimes(1);
-        expect(res.status).toHaveBeenCalledWith(201);
-        expect(res.json).toHaveBeenCalledWith({
-            message: "Appointment created successfully",
-            success: true,
-            status: 201,
-            data: {
-                id: '123e4567-e89b-12d3-a456-426614174000',
-                userId: "123e4567-e89b-12d3-a456-426614174000",
-                time: new Date().toISOString(),
-                staffId: "123e4567-e89b-12d3-a456-426614174000",
-                serviceId: "123e4567-e89b-12d3-a456-426614174000",
-                date : new Date(),
-                status: AppointmentStatus.PENDING
-            }
-        });
-    })
-})
+//         expect(mockSave).toHaveBeenCalledTimes(1);
+//         expect(res.status).toHaveBeenCalledWith(201);
+//         expect(res.json).toHaveBeenCalledWith({
+//             message: "Appointment created successfully",
+//             success: true,
+//             status: 201,
+//             data: {
+//                 id: '123e4567-e89b-12d3-a456-426614174000',
+//                 userId: "123e4567-e89b-12d3-a456-426614174000",
+//                 time: new Date().toISOString(),
+//                 staffId: "123e4567-e89b-12d3-a456-426614174000",
+//                 serviceId: "123e4567-e89b-12d3-a456-426614174000",
+//                 date : new Date(),
+//                 status: AppointmentStatus.PENDING
+//             }
+//         });
+//     })
+// })
